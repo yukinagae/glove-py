d50fa7440836cc1c6c44eaeecd9930579e87a227
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-06-06 20:11:49 +0900

    Remove some parsed arguments because some of them can be hard coded.

--- main.py
+++ main.py
@@ -5,49 +5,23 @@ logger = logging.getLogger("glove-py")
 
 
 def parse_args():
-    parser = ArgumentParser(
-        description=('Build a GloVe vector-space model from the '
-                     'provided corpus'))
-    parser.add_argument('corpus', metavar='corpus_path',
+    parser = ArgumentParser(description=(
+        'Build a GloVe vector-space model from the corpus'))
+    parser.add_argument('corpus',
+                        metavar='corpus_path',
                         type=lambda path: codecs.open(path, encoding='utf-8'))
 
     vocab_parser = parser.add_argument_group('Vocabulary options')
-    vocab_parser.add_argument('--vocab-path',
-                              help=('Path to vocabulary file. If this path '
-                                    'exists, the vocabulary will be loaded '
-                                    'from the file. If it does not exist, '
-                                    'the vocabulary will be written to this '
-                                    'file.'))
+    vocab_parser.add_argument('--vocab-path')
 
     cooccur_parser = parser.add_argument_group('Cooccurrence tracking options')
-    cooccur_parser.add_argument('--cooccur-path',
-                                help=('Path to cooccurrence matrix file. If '
-                                      'this path exists, the matrix will be '
-                                      'loaded from the file. If it does not '
-                                      'exist, the matrix will be written to '
-                                      'this file.'))
-    cooccur_parser.add_argument('-w', '--window-size', type=int, default=10,
-                                help=('Number of context words to track to '
-                                      'left and right of each word'))
-    cooccur_parser.add_argument('--min-count', type=int, default=10,
-                                help=('Discard cooccurrence pairs where at '
-                                      'least one of the words occurs fewer '
-                                      'than this many times in the training '
-                                      'corpus'))
+    cooccur_parser.add_argument('--cooccur-path')
+    cooccur_parser.add_argument('-w', '--window-size', type=int, default=10)
 
     glove_parser = parser.add_argument_group('GloVe options')
-    glove_parser.add_argument('--vector-path',
-                              help=('Path to which to save computed word '
-                                    'vectors'))
-    glove_parser.add_argument('-s', '--vector-size', type=int, default=100,
-                              help=('Dimensionality of output word vectors'))
-    glove_parser.add_argument('--iterations', type=int, default=25,
-                              help='Number of training iterations')
-    glove_parser.add_argument('--learning-rate', type=float, default=0.05,
-                              help='Initial learning rate')
-    glove_parser.add_argument('--save-often', action='store_true',
-                              default=False,
-                              help=('Save vectors after every training '))
+    glove_parser.add_argument('-s', '--vector-size', type=int, default=100)
+    glove_parser.add_argument('--iterations', type=int, default=25)
+    glove_parser.add_argument('--learning-rate', type=float, default=0.05)
 
     return parser.parse_args()
 
